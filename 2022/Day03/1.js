8801dc721d6d0dc51cd9e16d5b402d96de58adca
Hygoto <hygoto@hygoto.com>
2022-12-03 10:20:53 +0100

    2022 Day 3

---
+++ 2022/Day03/1.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: fs.createReadStream('./2022/Day03/input'),
+    crlfDelay: Infinity,
+  });
+
+  let sum = 0;
+  rl.on('line', (line) => {
+    let items = new Array(53).fill(0);
+    for (let index = 0; index < line.length/2; index++) {
+        if (line.charCodeAt(index) < 97) items[line.charCodeAt(index) - 38]++;
+        else items[line.charCodeAt(index) - 96]++;
+    }
+    for (let index = line.length/2; index < line.length; index++) {
+        if (line.charCodeAt(index) < 97 && items[line.charCodeAt(index) - 38] > 0) {
+            sum += line.charCodeAt(index) - 38;
+            index = line.length;
+        }
+        else if (items[line.charCodeAt(index) - 96] > 0) {
+            sum += line.charCodeAt(index) - 96;
+            index = line.length;
+        }
+    }
+  });
+  rl.on('close', () => console.log(sum));
---
+++ 2022/Day03/2.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: fs.createReadStream('./2022/Day03/input'),
+    crlfDelay: Infinity,
+  });
+
+  let sum = 0;
+  let elf = 0;
+  let badge = [];
+  let items = new Array(53).fill(0);
+  rl.on('line', (line) => {
+    if (elf === 0) {
+        for (let index = 0; index < line.length; index++) {
+            if (line.charCodeAt(index) < 97) items[line.charCodeAt(index) - 38]++;
+            else items[line.charCodeAt(index) - 96]++;
+        }
+        elf++;
+    }
+    else if (elf === 1) {
+        for (let index = 0; index < line.length; index++) {
+            if (line.charCodeAt(index) < 97 && items[line.charCodeAt(index) - 38] > 0) {
+                badge.push(line.charCodeAt(index) - 38);
+            }
+            else if (items[line.charCodeAt(index) - 96] > 0) {
+                badge.push(line.charCodeAt(index) - 96);
+            }
+        }
+        elf++;
+    }
+    else {
+        for (let index = 0; index < line.length; index++) {
+            badge.forEach(element => {
+                if (line.charCodeAt(index) < 97 && element === line.charCodeAt(index) - 38) {
+                    sum += line.charCodeAt(index) - 38;
+                    index = line.length;
+                }
+                else if (element === line.charCodeAt(index) - 96) {
+                    sum += line.charCodeAt(index) - 96;
+                    index = line.length;
+                }
+            });
+        }
+        elf = 0;
+        badge = [];
+        items.fill(0);
+    }
+  });
+  rl.on('close', () => console.log(sum));
