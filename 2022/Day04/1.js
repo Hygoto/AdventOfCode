98447a32b8f50aac2c3ec1531f831e299dcf3867
Hygoto <hygoto@hygoto.com>
2022-12-04 11:38:04 +0100

    2022 day 4

---
+++ 2022/Day04/1.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: fs.createReadStream('./2022/Day04/input'),
+    crlfDelay: Infinity,
+  });
+
+  let pairs = 0;
+  rl.on('line', (line) => {
+    let sections = line.split(/[-,]+/);
+    if (Math.sign(sections[0] - sections[2]) != Math.sign(sections[1] - sections[3] || (sections[0] - sections[2] === 0 && sections[1] - sections[3] === 0))) pairs++;
+});
+  rl.on('close', () => console.log(pairs));
